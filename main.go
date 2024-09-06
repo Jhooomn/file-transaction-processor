ec5b459cb8a18453fb07999337162aa5d148df84
Jhon Baron <jhooomn@gmail.com>
2024-09-06 16:53:36 -0500

    Set up lambda configuration and proper logger

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"os"
+
+	"github.com/Jhooomn/file-transaction-processor/utils"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/joho/godotenv"
+)
+
+func hello() (string, error) {
+	return "Hello λ!", nil
+}
 
 func main() {
-	fmt.Println("hello, there!")
+
+	logger := utils.NewLogger()
+
+	if os.Getenv("_LAMBDA_SERVER_PORT") == "" || os.Getenv("AWS_LAMBDA_RUNTIME_API") == "" {
+		if err := godotenv.Load(); err != nil {
+			logger.Error("Error loading .env file:", err)
+			return
+		}
+	}
+
+	logger.Info("Running the lambda!")
+	lambda.Start(hello)
 }
--- utils/utils.go
+++ utils/utils.go
@@ -1,1 +1,13 @@
 package utils
+
+import "github.com/sirupsen/logrus"
+
+func NewLogger() *logrus.Logger {
+	logger := logrus.New()
+	logger.SetLevel(logrus.InfoLevel)
+	logger.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp: true,
+	})
+
+	return logger
+}
